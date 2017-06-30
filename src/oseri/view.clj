7a82c6ccd60ee998a5e0a98ee54cb3ae3657b139
shinichi kogai <kogai0121@gmail.com>
2017-06-30 23:32:12 +0900

    fmt

--- src/oseri/view.clj
+++ src/oseri/view.clj
@@ -10,17 +10,16 @@
 (def direction #{:N :NE :E :SE :S :SW :W :NW})
 
 (def player (ref nil))
+
 (defn show-tile [x] (case (get x :color)
                       (:black) "B"
                       (:white) "W"
                       " "))
 
-(defn show-header-impl [size max]
-  (if (= size 0)
-    ""
-    (join [" " (- max size) (show-header-impl (- size 1) max)])))
+(defn show-header-impl [size max] (if (= size 0) ""
+                                      (join [" " (- max size) (show-header-impl (- size 1) max)])))
 
 (defn show-header [size] (show-header-impl size size))
 
 (defn show-line [line] (if (= (count line) 0) ""
-  (join [" " (show-tile (first line)) (show-line (rest line))])))
+                           (join [" " (show-tile (first line)) (show-line (rest line))])))
