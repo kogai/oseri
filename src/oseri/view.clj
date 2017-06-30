6a30989464d44c38781ad66ce1dfd629b0f3fb8d
shinichi kogai <kogai0121@gmail.com>
2017-06-30 23:13:50 +0900

    def show-header

--- src/oseri/view.clj
+++ src/oseri/view.clj
@@ -1,5 +1,5 @@
-(ns oseri.view)
-; (require '[clojure.core.match :refer [match]])
+(ns oseri.view
+  (:require [clojure.string :refer [join]]))
 
 (def board-size 8)
 
@@ -10,5 +10,16 @@
 (def direction #{:N :NE :E :SE :S :SW :W :NW})
 
 (def player (ref nil))
+(defn show-tile [x] (case (get x :color)
+                      (:black) "B"
+                      (:white) "W"
+                      " "))
+
+(defn show-header-impl [size max]
+  (if (= size 0)
+    ""
+    (join [" " (- max size) (show-header-impl (- size 1) max)])))
+
+(defn show-header [size] (show-header-impl size size))
 
 (defn show-line [line] "  ")
--- test/oseri/view_test.clj
+++ test/oseri/view_test.clj
@@ -6,6 +6,18 @@
   (testing "should be create board"
     (is (= (board 2) '(({:row 0 :column 0 :color :empty} {:row 0 :column 1 :color :empty}) ({:row 1, :column 0 :color :empty} {:row 1, :column 1 :color :empty}))))))
 
+(deftest test-show-tile
+  (testing "should be show black tile"
+    (is (= (show-tile {:color :black}) "B")))
+  (testing "should be show white tile"
+    (is (= (show-tile {:color :white}) "W")))
+  (testing "should be show empty tile"
+    (is (= (show-tile {:color :empty}) " "))))
+
+(deftest test-show-header
+  (testing "should be show header"
+    (is (= (show-header 3) " 0 1 2"))))
+
 (deftest test-show-line
   (testing "should be show first line"
     (is (= (show-line (first (board 1))) "  "))))
