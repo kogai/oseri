5c76309f1d0ab9f3b997cbf7823c0b1ffc942d13
shinichi kogai <kogai0121@gmail.com>
2017-07-04 09:51:02 +0900

    use anonymous function with implicitly arguments

--- src/oseri/model.clj
+++ src/oseri/model.clj
@@ -5,10 +5,10 @@
 ; (0,0)(0,1)(0,2)
 ; (1,0)(1,1)(1,2)
 ; (2,0)(2,1)(2,2)
-(defn successor [dir pos] (let [north (fn [p] (merge p {:row (- (get p :row) 1)}))
-                                east (fn [p] (merge p {:col (+ (get p :col) 1)}))
-                                south (fn [p] (merge p {:row (+ (get p :row) 1)}))
-                                west (fn [p] (merge p {:col (- (get p :col) 1)}))]
+(defn successor [dir pos] (let [north #(update % :row dec)
+                                east #(update % :col inc)
+                                south #(update % :row inc)
+                                west #(update % :col dec)]
                             ((case dir
                                (:N) north
                                (:NE) (comp north east)
